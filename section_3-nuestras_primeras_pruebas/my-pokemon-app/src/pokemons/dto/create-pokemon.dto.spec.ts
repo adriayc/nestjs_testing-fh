19e220437e4074cc1466805b5a42bd944ed3ca8f
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-03 19:26:03 -0400

    Otras pruebas sobre Create y Update Dtos

--- section_3-nuestras_primeras_pruebas/my-pokemon-app/src/pokemons/dto/create-pokemon.dto.spec.ts
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/src/pokemons/dto/create-pokemon.dto.spec.ts
@@ -34,4 +34,48 @@ describe('CreatePokemonDto', () => {
     const errorType = errors.find((error) => error.property === 'type');
     expect(errorType).toBeDefined();
   });
+
+  it('should hp must be positive number', async () => {
+    const dto = new CreatePokemonDto();
+    dto.name = 'pikachu';
+    dto.type = 'electric';
+    dto.hp = -10;
+
+    const errors = await validate(dto);
+    // console.log(errors);
+    const hpError = errors.find((error) => error.property === 'hp');
+    const constraints = hpError?.constraints;
+
+    expect(hpError).toBeDefined();
+    // expect(constraints).toBe({ min: 'hp must not be less than 0' }); // Error: Solo para tipos primitivos
+    expect(constraints).toEqual({ min: 'hp must not be less than 0' });
+  });
+
+  it('should be invalid with non-string sprites', async () => {
+    const dto = new CreatePokemonDto();
+    dto.name = 'pikachu';
+    dto.type = 'electric';
+    dto.sprites = [123, 456] as unknown as string[];
+
+    const errors = await validate(dto);
+    // console.log(errors);
+    const spritesError = errors.find((error) => error.property === 'sprites');
+
+    expect(spritesError).toBeDefined();
+  });
+
+  it('should be valid with string sprites', async () => {
+    const dto = new CreatePokemonDto();
+    dto.name = 'pikachu';
+    dto.type = 'electric';
+    dto.sprites = ['sprite1.png', 'sprite2.png'];
+
+    const errors = await validate(dto);
+    // console.log(errors);
+    const spritesError = errors.find((error) => error.property === 'sprites'); // No existe ningun error
+
+    // expect(spritesError).not.toBeDefined();
+    expect(spritesError).toBeUndefined();
+    expect(spritesError).toBe(undefined);
+  });
 });
