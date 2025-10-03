85fde28fcfac1547088e9d72375d93461fe0b19d
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-03 16:33:16 -0400

    Test - Pagination.dto (Parte 2)

--- section_3-nuestras_primeras_pruebas/my-pokemon-app/src/shared/dtos/pagination.dto.spec.ts
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/src/shared/dtos/pagination.dto.spec.ts
@@ -15,4 +15,51 @@ describe('PaginationDto', () => {
 
     expect(errors.length).toBe(0);
   });
+
+  it('should validate with valid data', async () => {
+    const dto = new PaginationDto();
+    dto.limit = 10;
+    dto.page = 2;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should not validate with invalid page', async () => {
+    const dto = new PaginationDto();
+    dto.page = -1;
+
+    const errors = await validate(dto);
+    // console.log(errors);
+
+    // expect(errors.length).toBe(1);
+    // expect(errors.length).toBeGreaterThanOrEqual(1);
+    errors.forEach((error) => {
+      if (error.property === 'page') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        // throw 'Esto no tuvo que suceder'; // No recomendado
+        // expect(true).toBe(false);
+        expect(true).toBeFalsy();
+      }
+    });
+  });
+
+  it('should not validate with invalid limit', async () => {
+    const dto = new PaginationDto();
+    dto.limit = -2;
+
+    const errors = await validate(dto);
+    // console.log(errors);
+
+    // expect(error.length).toBe(1);
+    errors.forEach((error) => {
+      if (error.property === 'limit') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
 });
