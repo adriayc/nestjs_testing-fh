1c25ad73c98c9dbceb9acb9ff28aa6e8331e3aaf
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-04 19:26:27 -0400

    Test - PokemonController

--- section_3-nuestras_primeras_pruebas/my-pokemon-app/src/pokemons/pokemons.controller.spec.ts
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/src/pokemons/pokemons.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
+import { PaginationDto } from 'src/shared/dtos/pagination.dto';
 
 describe('PokemonsController', () => {
   let controller: PokemonsController;
+  let service: PokemonsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,25 @@ describe('PokemonsController', () => {
     }).compile();
 
     controller = module.get<PokemonsController>(PokemonsController);
+    service = module.get<PokemonsService>(PokemonsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have called the service with correct parameter', async () => {
+    const dto: PaginationDto = { limit: 10, page: 1 };
+
+    // Permite observar el comportamiento de un método de un objeto
+    jest.spyOn(service, 'findAll');
+
+    await controller.findAll(dto);
+    // console.log(pokemons);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.findAll).toHaveBeenCalled();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.findAll).toHaveBeenCalledWith(dto);
   });
 });
