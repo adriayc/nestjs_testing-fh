d1dac47d243a069367b0c1c3fa41b696439180f7
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-06 18:17:59 -0400

    Test - Main.ts

---
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/src/main.spec.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { bootstrap } from './main';
+
+// Mock de libreria de terceros
+jest.mock('@nestjs/core', () => ({
+  NestFactory: {
+    create: jest.fn().mockResolvedValue({
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    }),
+  },
+}));
+
+describe('Main.ts Bootstrap', () => {
+  let mockApp: {
+    useGlobalPipes: jest.Mock;
+    setGlobalPrefix: jest.Mock;
+    listen: jest.Mock;
+  };
+
+  beforeEach(() => {
+    mockApp = {
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    };
+
+    (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
+  });
+
+  it('should create application', async () => {
+    await bootstrap();
+  });
+});
