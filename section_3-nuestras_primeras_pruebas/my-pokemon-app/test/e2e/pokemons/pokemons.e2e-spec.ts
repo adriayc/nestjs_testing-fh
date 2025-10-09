ec71f443c7b13a90ffbca29e04b54304c90a0b50
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-08 23:36:39 -0400

    E2E - Post: Con y sin body

--- section_3-nuestras_primeras_pruebas/my-pokemon-app/test/e2e/pokemons/pokemons.e2e-spec.ts
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/test/e2e/pokemons/pokemons.e2e-spec.ts
@@ -25,10 +25,50 @@ describe('Pokemons (e2e)', () => {
     await app.init();
   });
 
-  it('/pokemons (POST) - with no body', () => {
-    return request(app.getHttpServer())
-      .post('/pokemons')
-      .expect(200)
-      .expect('Hello World!');
+  it('/pokemons (POST) - with no body', async () => {
+    // Forma #1
+    // return request(app.getHttpServer())
+    //   .post('/pokemons')
+    //   .expect(200)
+    //   .expect('Hello World!');
+
+    // Forma #2
+    const response = await request(app.getHttpServer()).post('/pokemons');
+    // console.log(response);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const messageArray = response.body.message ?? [];
+
+    expect(response.statusCode).toBe(400);
+
+    expect(messageArray).toContain('name must be a string');
+    expect(messageArray).toContain('name should not be empty');
+    expect(messageArray).toContain('type must be a string');
+    expect(messageArray).toContain('type should not be empty');
+  });
+
+  it('/pokemons (POST) - with no body 2', async () => {
+    const mostHaveErrorMessage = [
+      'name must be a string',
+      'name should not be empty',
+      'type must be a string',
+      'type should not be empty',
+    ];
+
+    const response = await request(app.getHttpServer()).post('/pokemons');
+    // console.log(response);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const messageArray: string[] = response.body.message ?? [];
+
+    expect(messageArray.length).toBe(mostHaveErrorMessage.length);
+    expect(messageArray).toEqual(expect.arrayContaining(mostHaveErrorMessage));
+  });
+
+  it('/pokemons (POST - with valid body', async () => {
+    const response = await request(app.getHttpServer()).post('/pokemons').send({
+      name: 'pikachu',
+      type: 'electric',
+    });
   });
 });
