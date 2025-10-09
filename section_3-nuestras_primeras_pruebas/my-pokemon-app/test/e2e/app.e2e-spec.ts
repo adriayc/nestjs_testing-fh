f26e45f49a68ff5841a319b613a0c015c66b51e5
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-08 23:36:07 -0400

    E2E - Post: Crear pokemon

--- section_3-nuestras_primeras_pruebas/my-pokemon-app/test/e2e/app.e2e-spec.ts
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/test/e2e/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 // import * as request from 'supertest';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
---
+++ section_3-nuestras_primeras_pruebas/my-pokemon-app/test/e2e/pokemons/pokemons.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+// import * as request from 'supertest';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from '../../../src/app.module';
+
+describe('Pokemons (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+    // app.setGlobalPrefix('api');
+
+    await app.init();
+  });
+
+  it('/pokemons (POST) - with no body', () => {
+    return request(app.getHttpServer())
+      .post('/pokemons')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
