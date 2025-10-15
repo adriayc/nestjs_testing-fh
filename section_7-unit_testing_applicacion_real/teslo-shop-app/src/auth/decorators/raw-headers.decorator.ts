bc0808d15155d224e7f96f3694a582cc7dc5bd98
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-15 18:37:36 -0400

    Test - RawHeaders Decorator

--- section_7-unit_testing_applicacion_real/teslo-shop-app/src/auth/decorators/raw-headers.decorator.ts
+++ section_7-unit_testing_applicacion_real/teslo-shop-app/src/auth/decorators/raw-headers.decorator.ts
@@ -1,12 +1,15 @@
-import {
-  createParamDecorator,
-  ExecutionContext,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const RawHeaders = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.rawHeaders;
-  },
-);
+export const getRawHeaders = (data: string, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  return req.rawHeaders; // [Authorization, bearer token, user-agent]
+};
+
+// export const RawHeaders = createParamDecorator(
+//   (data: string, ctx: ExecutionContext) => {
+//     const req = ctx.switchToHttp().getRequest();
+//     return req.rawHeaders; // [Authorization, bearer token, user-agent]
+//   },
+// );
+
+export const RawHeaders = createParamDecorator(getRawHeaders);
