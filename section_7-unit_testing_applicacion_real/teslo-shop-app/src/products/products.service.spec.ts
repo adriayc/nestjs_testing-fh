3fdd5030c47d099c5fb4ebbcc929227d15249e70
Adriano Ayala <adriano.ayc@gmail.com>
2025-10-22 17:29:39 -0400

    Test - Creación de productos

--- section_7-unit_testing_applicacion_real/teslo-shop-app/src/products/products.service.spec.ts
+++ section_7-unit_testing_applicacion_real/teslo-shop-app/src/products/products.service.spec.ts
@@ -2,10 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
 import { Product, ProductImage } from './entities';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { CreateProductDto } from './dto/create-product.dto';
 
 describe('Product Service', () => {
   let service: ProductsService;
+  let productRepository: Repository<Product>;
+  let productImageRepository: Repository<ProductImage>;
 
   beforeEach(async () => {
     const mockProductRepository = {
@@ -59,9 +63,53 @@ describe('Product Service', () => {
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    productRepository = module.get<Repository<Product>>(
+      getRepositoryToken(Product),
+    );
+    productImageRepository = module.get<Repository<ProductImage>>(
+      getRepositoryToken(ProductImage),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a product', async () => {
+    const dto = {
+      title: 'Test Product',
+      price: 100,
+      images: ['imag1.png'],
+    } as CreateProductDto;
+
+    const { images: dtoWithNoImages, ...createDto } = dto;
+
+    const user = {
+      id: '1',
+      email: 'test@mail.com',
+    } as User;
+
+    const product = {
+      id: '1',
+      ...createDto,
+      user,
+    } as unknown as Product;
+
+    jest.spyOn(productRepository, 'create').mockReturnValue(product);
+    jest.spyOn(productRepository, 'save').mockResolvedValue(product);
+    jest
+      .spyOn(productImageRepository, 'create')
+      .mockImplementation((imageData) => imageData as unknown as ProductImage);
+
+    const result = await service.create(dto, user);
+    // console.log(result);
+
+    expect(result).toEqual({
+      id: '1',
+      title: 'Test Product',
+      price: 100,
+      images: ['imag1.png'],
+      user: { id: '1', email: 'test@mail.com' },
+    });
   });
 });
